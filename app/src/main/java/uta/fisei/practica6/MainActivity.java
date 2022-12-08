d6fde64ac4d2761c22dfdfa19f7d033056e1bad0
Jeanpierre Sarzosa <jj.sarzosa@gmail.com>
2022-12-08 11:04:49 -0500

    Initial commit

---
+++ app/src/main/java/uta/fisei/practica6/MainActivity.java
@@ -0,0 +1,17 @@
+package uta.fisei.practica6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+    // called when the app first launches
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
